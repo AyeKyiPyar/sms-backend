551440431d0c9412e7ff2889894c1fc79dfb2281
KyiPyar <ayekyipyarshwe@gmail.com>
2026-03-28 15:09:55 +0630

    update webconfig

--- src/main/java/com/akps/demo/config/WebConfig.java
+++ src/main/java/com/akps/demo/config/WebConfig.java
@@ -9,10 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:5173") // or your frontend URL
-                .allowCredentials(true)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+		    	registry.addMapping("/**")
+		        .allowedOriginPatterns("*")   // ✅ works with credentials
+		        .allowedMethods("*")
+		        .allowCredentials(true);
     }
 }
